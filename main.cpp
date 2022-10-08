623f51596e7da148bd3b53ea9c394034f5ab50e3
İBRAHİM BERK <102363550+berkibrahi@users.noreply.github.com>
2022-10-08 21:35:53 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+class nokta{
+	public:
+		int x,y;
+};
+
+int main(int argc, char** argv) {
+nokta n;
+n.x=4;
+n.y=8;
+cout<<n.x<< "," <<n.y<<endl;
+
+
+	return 0;
+}
